d5def7f5435e5d9e63e21d6bc51f95dcb6605691
mbmetcalfe <michael.metcalfe@gmail.com>
2019-07-20 15:19:13 -0300

    Adding new archer arrow enhancements

--- tfscripts/archer.tf
+++ tfscripts/archer.tf
@@ -52,7 +52,7 @@
         %{tAction} %{mobDir} %{longshotMob} %{avs_spot}%;\
     /endif
 
-/def -mregexp -t'A brace of ([a-zA-Z]+) (bolt|arrow|sling stone)s (is here|are la?ying on the ground)\.' grabarrowtrig = \
+/def -mregexp -t'A brace of ([a-zA-Z ]+) (bolt|arrow|sling stone)s (is here|are la?ying on the ground)\.' grabarrowtrig = \
     /let arrowType=%{P2}%;\
     /if (({autoloot}=1) & (regmatch({myclass},{arcType}))) \
         /send get "%{arrowType}"%;\
@@ -109,7 +109,7 @@
     /let _projectileType=$[strip_attr({P2})]%;\
     /if ({_projectileType} =~ "sling stones") /let _projectileType=stones%;/endif%;\
     /if ({autofletch} == 1) \
-        /def full_mana_action = wa;fletch %{_projectileType} %{_arrowType}%;\
+        /def full_mana_action = /echo -pw [full_mana_action] @{Cyellow}fletch %{_projectileType} '%{arrowlevel} %{_arrowType}'%;stand;fletch %{_projectileType} '%{arrowlevel} %{_arrowType}'%;\
         /send sleep%;\
     /endif
 
@@ -117,37 +117,46 @@
     /set tofletch=$[--tofletch] %; \
     /set totalfletched=$[totalfletched+numfletched]%; \
     /if ({autofletch} == 1 & {tofletch} > 0) \
-        fletch %fletchtype %arrowtype%; \
+        /echo -pw [archer_autofletch_nothing] @{Cyellow}fletch %fletchtype '%arrowlevel %arrowtype'%;\
+        fletch %fletchtype '%arrowlevel %arrowtype'%;\
         /if (mod(tofletch,5) == 0) /_varietyCommand%;/endif%;\
     /else \
         /send rem fletch=put fletch %{main_bag}=wear %unbrandish%; \
     /endif%; \
     /flstat %{fletchtype}s
 
+;fletch bolts 'lordly piercing'
+;Your efforts produced 1 lordly explosive arrow
+;Your efforts produced 1 explosive arrow
 /def -mregexp -t"Your efforts produced ([0-9]+) (.*) (arrow|bolt|stone)s?" archer_autofletch = \
     /let numfletched=%P1%; \
     /set tofletch=$[--tofletch] %; \
     /set totalfletched=$[totalfletched+numfletched]%; \
     /if ({autofletch} == 1 & {tofletch} > 0) \
-        fletch %fletchtype %arrowtype%; \
+        /echo -pw [archer_autofletch] @{Cyellow}%fletchtype '%arrowlevel %arrowtype'%;\
+        fletch %fletchtype '%arrowlevel %arrowtype'%;\
         /if (mod(tofletch,5) == 0) /_varietyCommand%;/endif%;\
     /else \
         /send rem fletch=put fletch %{main_bag}=wear %unbrandish%; \
     /endif%; \
     /flstat %{P2} %{P3}s
+
 /def -i -mglob -t"You discard your empty toolkit." archer_nofletchkit = \
     /if ({autofletch} = 1 & {tofletch} > 0) \
-        /send get fletch %{main_bag}=wear fletch=fletch %fletchtype %arrowtype%; \
+        /echo -pw [archer_nofletchkit] @{Cyellow}fletch %fletchtype '%arrowlevel %arrowtype'%;\
+        /send get fletch %{main_bag}=wear fletch=fletch %fletchtype '%arrowlevel %arrowtype'%; \
     /endif
 
 /alias mkarrow \
-    /if ({#} != 3) \
-        /echo -p %%% @{hCred}Syntax: @{nCwhite}mkarrow @{xCyellow}<num> <arrow|bolt|stone> <type>%; \
+    /if ({#} < 3) \
+        /echo -p %%% @{hCred}Syntax: @{nCwhite}mkarrow @{xCyellow}<num> <arrow|bolt|stone> <type> [lordly|heroic]%; \
     /else \
         /test fletchStart := (time())%; \
         /set autofletch=1%;/set totalfletched=0%; \
         /set tofletch=%1%;/set fletchtype=%2%;/set arrowtype=%3%; \
-        /send get fletch %{main_bag}=wear fletch=fletch %fletchtype %arrowtype%; \
+        /if ({#} == 4) /set arrowlevel=%{4}%;/else /unset arrowlevel%;/endif%;\
+        /echo -pw [mkarrow] @{Cyellow}fletch %fletchtype '%arrowlevel %arrowtype'%; \
+        /send get fletch %{main_bag}=wear fletch=fletch %fletchtype '%arrowlevel %arrowtype'%; \
     /endif
 
 /def flstat = \
